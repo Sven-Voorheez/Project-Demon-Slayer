dc23392fbd99ad04b6693041407114a6c471c53a
Eldor <eldorrasulmuhamedov@gmail.com>
2024-08-30 23:55:18 +0500

    test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders navigation and routes', () => {
+  render(
+    <Router>
+      <App />
+    </Router>
+  );
+  
 });
