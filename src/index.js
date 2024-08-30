fe0485633a300008e6097b823d1206b1204cb9f1
Eldor <eldorrasulmuhamedov@gmail.com>
2024-08-31 00:29:34 +0500

    Up date

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { HashRouter } from 'react-router-dom'; // Импорт HashRouter
+import { HashRouter } from 'react-router-dom'; 
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter> {/* Оборачиваем App в HashRouter */}
+    <HashRouter> 
       <App />
     </HashRouter>
   </React.StrictMode>
